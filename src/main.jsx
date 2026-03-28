48ce6a9a5130d18fda1ad6c1a8902d3492e8a6b2
omarapdelgwad44 <omarapdelgwad44@gmail.com>
2026-03-28 08:10:25 +0200

    set api setting

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
 
+// --- إعدادات Axios ---
+import axios from 'axios';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+// --------------------
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
